419b41641cccf90d8004705fcd4f8d1329c8c062
Nacho Puerto <nachopuerto95@gmail.com>
2022-04-09 20:45:32 +0200

    menu hamburguesa

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 const navToggle = document.querySelector(".toggle");
-const navMenu = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav--opacity");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav--visible");
-  
+
 });
 
-
+navMenu.addEventListener("click", () => {
+    navMenu.classList.remove("nav--visible");
+});
 
 
